002ec61e01b7693146b9010e84695204062e7743
Benjamin Brobæk Lindgaard <benj2421@edu.easj.dk>
2017-09-29 09:59:32 +0200

    design

--- DesignTilTidsreg/view/Login.xaml.cs
+++ DesignTilTidsreg/view/Login.xaml.cs
@@ -13,18 +13,28 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace DesignTilTidsreg
+namespace DesignTilTidsreg.view
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BrugerPage));
         }
     }
 }
